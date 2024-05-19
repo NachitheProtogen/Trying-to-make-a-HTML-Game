6fa60b489ab153281b84e60ae04e5a471a1da82e
Nachi <nils.christian2006@gmail.com>
2024-05-19 17:09:38 +0200

    Starting to have something that used to work

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
+let score = 0;
+let scoreHtml = document.getElementById("scoreHtml");
 
 function game() {
-    let score = 0;
+    score = 0;
+    let timer = 0;
 
+    let game = setInterval(function(){
+        if(target){
+            field.removeChild(target);
+        };
+        CreateTarget();
+        timer++;
+        if(timer=20){
+            clearInterval(game);
+        }
+    },1000)
+};
+
+function CreateTarget() {
     let target = document.createElement("div");
     target.style.width = "40px";
     target.style.height = "40px";
     target.style.borderRadius = "50%";
-    target.style.position = "absolute";
-    target.style.left = Math.random();
-    target.style.top = Math.random();
+    target.style.position = "relative";
+    target.style.backgroundColor = "red"
+    target.style.left = Math.floor(Math.random() * (400 - 5 + 1)) + 5 + "px";
+    target.style.top = Math.floor(Math.random() * (300 - 5 + 1)) + 5 + "px";
     target.id = "target"
     target.addEventListener("click", () => {
-        document.removeChild(target);
-        score + 50;
+        field.removeChild(target);
+        score = score + 50;
+        scoreHtml.value = score;
     })
+    field.appendChild(target);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    scoreHtml.disabled = true;
+})
+
+start.addEventListener("click", () => {
+    game();
+});
