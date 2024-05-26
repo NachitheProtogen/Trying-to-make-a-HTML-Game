545ec16d1126d6f1332d0cd94354dd07a48dd067
NachitheProtogen <nils.christian2006@gmail.com>
2024-05-26 16:21:22 +0200

    Comment seption

--- index.js
+++ index.js
@@ -4,21 +4,14 @@ let scoreHtml = document.getElementById("scoreHtml");
 function game() {
     score = 0;
     let timer = 0;
-    clearInterval(game);
 
-    let game = setInterval(function(){
-        CreateTarget();
-        timer++;
-        if(timer=20){
-            clearInterval(game);
-        }
-    },1000)
+    CreateTarget();
 };
 
 function CreateTarget() {
-    let target = document.createElement("div");
-    target.style.width = "40px";
-    target.style.height = "40px";
+    let target = document.createElement("div"); //insanity
+    target.style.width = "80px";
+    target.style.height = "80px";
     target.style.borderRadius = "50%";
     target.style.position = "relative";
     target.style.backgroundColor = "red"
@@ -29,10 +22,43 @@ function CreateTarget() {
         field.removeChild(target);
         score = score + 50;
         scoreHtml.value = score;
+        CreateTarget();
     })
-    field.removeChild(target);
+    let targetyel = document.createElement("div"); //why am I doing this?
+    targetyel.style.width = "50px";
+    targetyel.style.height = "50px";
+    targetyel.style.borderRadius = "50%";
+    targetyel.style.position = "relative";
+    targetyel.style.backgroundColor = "yellow";
+    targetyel.style.left = "15px";
+    targetyel.style.top = "15px";
+    targetyel.id = "targetyel"
+    targetyel.addEventListener("click", () => {
+        field.removeChild(target);
+        score = score + 100;
+        scoreHtml.value = score;
+        CreateTarget();
+    })
+    let targetgoat = document.createElement("div"); //fucking hate this stupif j9s8hjd0jmsa0s8d9zhsad
+    targetgoat.style.width = "20px";
+    targetgoat.style.height = "20px";
+    targetgoat.style.borderRadius = "50%";
+    targetgoat.style.position = "relative";
+    targetgoat.style.backgroundColor = "green";
+    targetgoat.style.left = "15px";
+    targetgoat.style.top = "15px";
+    targetgoat.id = "targetgoat"
+    targetgoat.addEventListener("click", () => {
+        field.removeChild(target);
+        score = score + 150;
+        scoreHtml.value = score;
+        CreateTarget();
+    })
+
     field.appendChild(target);
-}
+    target.appendChild(targetyel);
+    targetyel.appendChild(targetgoat);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
     scoreHtml.disabled = true;
